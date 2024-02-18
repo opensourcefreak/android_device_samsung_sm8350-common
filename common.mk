bba3f78cf73ff8cd086e80c60de73c5fb0eb3ab0
LucasBlackLu <lucasblack997@gmail.com>
2024-02-18 10:14:30 +0800

    sm8350-common:  Intial device configurations

---
+++ common.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Enable project quotas and casefolding for emulated storage without sdcardfs
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
+# Fastbootd
+PRODUCT_PACKAGES += \
+	android.hardware.fastboot@1.1-impl-mock \
+    fastbootd
+
+# Init
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default \
+    $(LOCAL_PATH)/init/fstab.default:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.default
+
+PRODUCT_PACKAGES += \
+    fstab.default
+
+# Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
